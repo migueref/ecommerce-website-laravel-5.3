f62bda7f5ec1bcd600976c4384703b0210b6c2b5
migueref <rfmo126055@upemor.edu.mx>
2017-04-08 18:11:23 -0500

    Added store for products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+
+use Illuminate\Support\Facades\Auth;
 class ProductsController extends Controller
 {
     /**
@@ -35,7 +37,16 @@ class ProductsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $product = new Product;
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->pricing = $request->pricing;
+        $product->user_id = Auth::user()->id;
+        if($product->save()){
+          return redirect("/products");
+        }else{
+          return view("products.create");
+        }
     }
 
     /**
